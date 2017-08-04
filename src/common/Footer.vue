81862ec3ece9bf9f4b6431bd9971f0e4a5ddc0ee
RenTao <912683552@qq.com>
2017-08-04 15:36:08 +0800

    add Bottom Navigation

---
+++ src/common/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+    <mu-paper>
+        <mu-bottom-nav :value="bottomNav" shift @change="handleChange" class = "footer">
+            <mu-bottom-nav-item value="movies" title="Movies" icon="ondemand_video"/>
+            <mu-bottom-nav-item value="music" title="Music" icon="music_note"/>
+            <mu-bottom-nav-item value="books" title="Books" icon="books"/>
+            <mu-bottom-nav-item value="pictures" title="Pictures" icon="photo"/>
+            <mu-bottom-nav-item value="person" title="Person" icon="person"/>
+        </mu-bottom-nav>
+    </mu-paper>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                bottomNav: 'movies'
+        }
+    },
+    methods: {
+        handleChange (val) {
+            this.bottomNav = val
+        }
+    }
+}
+</script>
+
+<style lang="less">
+    .footer{
+        position: fixed;
+        bottom: 0px;
+        left: 0px;
+        width: 100%;
+        background-color: #ff9800;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
-import Vue from 'vue'
+import Vue from 'vue';
+import App from './App';
+import MuseUI from 'muse-ui';
+import 'muse-ui/dist/muse-ui.css';
+import 'muse-ui/dist/theme-carbon.css'
+
+
+Vue.use(MuseUI)
+
+Vue.config.productionTip = false;//关闭生产模式下给出的提示
+
+new Vue({
+    el:"#app",
+    render:h=>h(App)
+})
